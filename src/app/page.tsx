5a439a82b3ae2c0370c835fb1e49b75a1495a113
devahmad-001 <117432911+789farhan@users.noreply.github.com>
2025-04-28 09:02:34 +0500

    props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Button from "@/components/button/button";
 
 export default function Home() {
   return (
     <>
       <h1>Home page </h1>
+      <Button title="Click here"/>
     </>
   );
 }
